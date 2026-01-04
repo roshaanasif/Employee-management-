8f3ca8697d04d42f63232e017e332461ab34ce24
Roshaan Asif <roshaanasif57@gmail.com>
2026-01-05 00:23:42 +0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const notyf = new Notyf();
+// let addContent=document.getElementsByClassName(".adding");
 let addBtn=document.getElementById("add");
+let editBtn=document.getElementById("edit");
 let employeeTable=document.getElementById("employeeTable")
 let search=document.getElementById("searchInput");
 let searchBtn=document.getElementById("searchbtn");
 let employeeInfo = JSON.parse(localStorage.getItem("employees")) || {};  
 let deleteBtns = document.querySelectorAll(".deleteBtn");
+let editBtns = document.querySelectorAll(".editBtn");
  
   
 
@@ -28,6 +31,7 @@ if (Object.keys(employeeInfo).length === 0) {
             <td class="px-6 py-3 text-gray-800">${employeeInfo[key].employeeJob}</td>
             <td class="px-6 py-3 text-gray-800">${employeeInfo[key].employeeSalaray}</td>
             <td class="px-6 py-3 text-gray-800"><i class="deleteBtn fa-solid fa-trash text-red-500 cursor-pointer hover:text-red-700" onclick="deleteEmployee('${employeeInfo[key].employeeName}')"></i></td>
+            <td class="px-6 py-3 text-gray-800"><i class="editBtn fa-regular fa-pen-to-square text-green-500 cursor-pointer hover:text-green-700" onclick="gettingEmployeeInfo('${employeeInfo[key].employeeName}')"></i></td>
             </tr>
             `
     
@@ -61,10 +65,8 @@ document.getElementById("name").value="";
 }
 
 let gettingData=()=>{
+
 employeeTable.innerHTML="";
- 
-
-
 if (Object.keys(employeeInfo).length === 0) {
      employeeTable.innerHTML+=`
             <tr>
@@ -85,7 +87,8 @@ if (Object.keys(employeeInfo).length === 0) {
                   <td class="px-6 py-3 text-gray-800">${employeeInfo[key].employeeJob}</td>
                   <td class="px-6 py-3 text-gray-800">${employeeInfo[key].employeeSalaray}</td>
                   <td class="px-6 py-3 text-gray-800"><i class="deleteBtn fa-solid fa-trash text-red-500 cursor-pointer hover:text-red-700" id="${employeeInfo[key].employeeName}" "></i></td>
-    
+                <td class="px-6 py-3 text-gray-800"><i class="editBtn fa-regular fa-pen-to-square text-green-500 cursor-pointer hover:text-green-700" onclick="gettingEmployeeInfo('${employeeInfo[key].employeeName}')"></i></td>
+
               </tr>
           `
     }
@@ -136,7 +139,7 @@ let searchEmployee=()=>{
 searchBtn.addEventListener("click",searchEmployee);
     
 
-
+let findingEmployee;
 
 let deleteEmployee=item=>{
 
@@ -146,15 +149,128 @@ let deleteEmployee=item=>{
            console.log(employeeInfo);
            localStorage.setItem("employees", JSON.stringify(employeeInfo));
            notyf.error("Employee deleted !!")
-           gettingData();
+        }
+    }
+    gettingData();
+
+}
+let gettingEmployeeInfo=item=>{
+    console.log(item);
+  
+
+    addBtn.classList.add("hidden");
+    editBtn.classList.remove("hidden");
+    let employee=document.getElementById("name");
+    let Job=document.getElementById("job");
+    let City=document.getElementById("city");
+    let Salaray =document.getElementById("salary");
+    
+    for (let key in employeeInfo){
+        if(employeeInfo[key].employeeName==item){
+            console.log(employeeInfo[key].employeeCity);
+            
+            employee.value=employeeInfo[key].employeeName;
+            Job.value=employeeInfo[key].employeeJob;
+            City.value=employeeInfo[key].employeeCity;
+            Salaray.value=employeeInfo[key].employeeSalaray;
+
+            findingEmployee=employeeInfo[key].employeeName;
+            console.log(findingEmployee);
+            
+            
+        //    localStorage.setItem("employees", JSON.stringify(employeeInfo));
+        //    notyf.error("Employee deleted !!")
+        //    gettingData();
+
         }
     }
 
 }
 
+
+let editEmployee=(findingEmployee)=>{
+
+ console.log(findingEmployee);
+ 
+   let empName=document.getElementById("name").value;
+   let Job=document.getElementById("job").value;
+   let City=document.getElementById("city").value;
+   let Salary =document.getElementById("salary").value;
+   
+
+ if(empName && City && Job && Salary){
+    for (let key in employeeInfo){
+        if(employeeInfo[key].employeeName==findingEmployee){
+
+            if(employeeInfo[key].employeeName==empName && employeeInfo[key].employeeCity==City && employeeInfo[key].employeeJob==Job && employeeInfo[key].employeeSalaray==Salary) {
+              notyf.error("YOU EDIT NOTHING"); 
+              document.getElementById("name").value="";
+          document.getElementById("job").value="";
+          document.getElementById("city").value="";
+          document.getElementById("salary").value="";
+              return 
+            }
+
+           if (employeeInfo[key].employeeName!==empName) {
+             delete employeeInfo[key];
+
+             employeeInfo[empName] = {
+                employeeName: empName,
+                employeeCity: City,
+                employeeJob: Job,
+                employeeSalaray: Salary
+                };
+                            
+             console.log(employeeInfo);
+             
+            }else{
+                employeeInfo[key].employeeName=empName;
+                employeeInfo[key].employeeJob=Job;
+                employeeInfo[key].employeeCity=City;
+                employeeInfo[key].employeeSalaray=Salary;
+            }   
+
+            localStorage.setItem("employees",JSON.stringify(employeeInfo)); 
+            notyf.success("Employee edit Successfully");
+            
+            
+        }
+        
+      
+
+}
+
+          document.getElementById("name").value="";
+          document.getElementById("job").value="";
+          document.getElementById("city").value="";
+          document.getElementById("salary").value="";
+            gettingData();
+
+}else{
+    notyf.error("Fill out all fields")
+}
+
+ addBtn.classList.remove("hidden");
+    editBtn.classList.add("hidden");
+
+}
+   
+
+
+editBtn.addEventListener("click", () => {
+  editEmployee(findingEmployee);
+});
+
+
 deleteBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       deleteEmployee(btn.id);
+    });
+  });
+
+editBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      gettingEmployeeInfo(btn.id);
     });
   });
 
